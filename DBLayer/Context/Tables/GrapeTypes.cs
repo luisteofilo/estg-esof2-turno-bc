75829d62241d000cb6d94e7a2f06e237f7f671ac
Hugo Guedes <106330996+HugoGuedesipvc@users.noreply.github.com>
2024-06-20 21:16:26 +0100

    Wine: Entities, CRUD, Endpoints (#21)
    * Creation of entitie Wine and is relations entites
    
    * Revert "Creation of entitie Wine and is relations entites"
    
    This reverts commit 3cea131d438e48c53f1c58e3d2c140658c6aff8b.
    
    * Reapply "Creation of entitie Wine and is relations entites"
    
    This reverts commit 7d5db4aef79de9d2fc9bfa0980204d9a0a2de872.
    
    * Creation of Wine and is relations
    
    * Delete .env
    
    * no message
    
    * no message
    
    * no message
    
    * no message

---
+++ DBLayer/Context/Tables/GrapeTypes.cs
@@ -0,0 +1,19 @@
+﻿using ESOF.WebApp.DBLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+namespace ESOF.WebApp.DBLayer.Context;
+
+public partial class ApplicationDbContext
+{
+
+    private void BuildGrapeTypes(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<GrapeType>()
+            .Property(p => p.GrapeTypeId)
+            .HasDefaultValueSql("gen_random_uuid()");
+        
+        modelBuilder.Entity<GrapeType>()
+            .HasMany(gt => gt.WineGrapeTypes)
+            .WithOne(wgt => wgt.GrapeType)
+            .HasForeignKey(wgt => wgt.GrapeTypeId);
+    }
+}
---
+++ WebAPI/DtoClasses/UpdateBrandDto.cs
@@ -0,0 +1,7 @@
+﻿namespace ESOF.WebApp.WebAPI.DtoClasses;
+
+public class UpdateBrandDto
+{
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+}
