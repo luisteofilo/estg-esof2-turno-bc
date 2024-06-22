c94b95f839dfe6c7fd2a25afff4cd375c5292969
João Ferreira <j.pedro.ferreira@ipvc.pt>
2024-06-23 00:05:37 +0100

    add: taste question

---
+++ DBLayer/Context/Tables/TasteQuestions.cs
@@ -0,0 +1,21 @@
+using ESOF.WebApp.DBLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+
+// ReSharper disable once CheckNamespace
+namespace ESOF.WebApp.DBLayer.Context;
+
+public partial class ApplicationDbContext
+{
+    private void BuildTasteQuestions(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<TasteQuestion>(entity =>
+        {
+            entity.Property(p => p.TasteQuestionId)
+                .HasDefaultValueSql("gen_random_uuid()");
+
+            entity.HasOne(te => te.TasteQuestionType)
+                .WithMany()
+                .HasForeignKey(te => te.TasteQuestionTypeId);
+        });
+    }
+}
