7a3f0892c4fb0cd9e9d1d0cc257f025857b613c8
João Ferreira <j.pedro.ferreira@ipvc.pt>
2024-06-23 00:05:31 +0100

    add: taste evaluation

---
+++ DBLayer/Context/Tables/TasteEvaluations.cs
@@ -0,0 +1,25 @@
+using ESOF.WebApp.DBLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+
+// ReSharper disable once CheckNamespace
+namespace ESOF.WebApp.DBLayer.Context;
+
+public partial class ApplicationDbContext
+{
+    private void BuildTasteEvaluations(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<TasteEvaluation>(entity =>
+        {
+            entity.Property(p => p.TasteEvaluationId)
+                .HasDefaultValueSql("gen_random_uuid()");
+
+            entity.HasOne(te => te.User)
+                .WithMany()
+                .HasForeignKey(te => te.UserId);
+
+            entity.HasOne(te => te.Wine)
+                .WithMany()
+                .HasForeignKey(te => te.WineId);
+        });
+    }
+}
---
+++ DBLayer/Entities/TasteEvaluation.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ESOF.WebApp.DBLayer.Entities;
+
+public class TasteEvaluation
+{
+    [Key]
+    public Guid TasteEvaluationId { get; set; }
+
+    public Guid UserId { get; set; }
+    public User User { get; set; }
+    
+    public Guid WineId { get; set; }
+    public Wine Wine { get; set; }
+    
+    // public Guid EventId { get; set; }
+    // public Event Event { get; set; }
+    
+    public int WineScore { get; set; }
+}
