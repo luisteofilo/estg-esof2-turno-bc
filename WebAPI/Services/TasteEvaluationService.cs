82ce73ac633a3a50d34ca911b975a585ac88ce77
João Ferreira <j.pedro.ferreira@ipvc.pt>
2024-06-23 01:02:15 +0100

    add: taste evaluation service

---
+++ WebAPI/Services/TasteEvaluationService.cs
@@ -0,0 +1,200 @@
+using ESOF.WebApp.WebAPI.DtoClasses;
+
+namespace ESOF.WebApp.WebAPI.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using ESOF.WebApp.DBLayer.Entities;
+using ESOF.WebApp.DBLayer.Context;
+using Microsoft.EntityFrameworkCore;
+
+public class TasteEvaluationService
+{
+    private readonly ApplicationDbContext _context;
+
+    public TasteEvaluationService(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    /**
+     *
+     * Get all taste evaluations.
+     *
+     * @return The taste evaluations.
+     * 
+     */
+    public List<ResponseTasteEvaluationDto> GetAll()
+    {
+        try
+        {
+            return _context.TasteEvaluations.Select(tasteEvaluation => new ResponseTasteEvaluationDto
+            {
+                TasteEvaluationId = tasteEvaluation.TasteEvaluationId,
+                UserId = tasteEvaluation.UserId,
+                WineId = tasteEvaluation.WineId,
+                
+                // TODO: Add the event
+                // EventId = tasteEvaluation.EventId,
+                
+                WineScore = tasteEvaluation.WineScore
+            }).ToList();
+        }
+        catch (Exception ex)
+        {
+            throw new Exception("Error: An error occurred while retrieving taste evaluations.", ex);
+        }
+    }
+
+    /**
+     *
+     * Get a taste evaluation by the given id.
+     *
+     * @param id The id of the taste evaluation to get.
+     *
+     * @return The taste evaluation.
+     * 
+     */
+    public ResponseTasteEvaluationDto Get(Guid id)
+    {
+        var tasteEvaluation = _context.TasteEvaluations.Find(id);
+        if (tasteEvaluation == null)
+        {
+            throw new ArgumentException("Error: That taste evaluation does not exist!");
+        }
+
+        return new ResponseTasteEvaluationDto
+        {
+            TasteEvaluationId = tasteEvaluation.TasteEvaluationId,
+            UserId = tasteEvaluation.UserId,
+            WineId = tasteEvaluation.WineId,
+            
+            // TODO: Add the event
+            // EventId = tasteEvaluation.EventId,
+            
+            WineScore = tasteEvaluation.WineScore
+        };
+    }
+
+    /**
+     *
+     * Create a taste evaluation based on the given data.
+     *
+     * @param createTasteEvaluationDto The data to create the taste evaluation with.
+     *
+     * @return The created taste evaluation.
+     * 
+     */
+    public ResponseTasteEvaluationDto Create(CreateTasteEvaluationDto createTasteEvaluationDto)
+    {
+        try
+        {
+            var tasteEvaluation = new TasteEvaluation
+            {
+                UserId = createTasteEvaluationDto.UserId,
+                WineId = createTasteEvaluationDto.WineId,
+                
+                // TODO: Add the event
+                // EventId = createTasteEvaluationDto.EventId,
+                
+                WineScore = createTasteEvaluationDto.WineScore
+            };
+
+            _context.TasteEvaluations.Add(tasteEvaluation);
+            _context.SaveChanges();
+
+            return new ResponseTasteEvaluationDto
+            {
+                TasteEvaluationId = tasteEvaluation.TasteEvaluationId,
+                UserId = tasteEvaluation.UserId,
+                WineId = tasteEvaluation.WineId,
+                
+                // TODO: Add the event
+                // EventId = tasteEvaluation.EventId,
+                
+                WineScore = tasteEvaluation.WineScore
+            };
+        }
+        catch (DbUpdateException ex)
+        {
+            throw new Exception("Error: An error occurred while creating the TasteEvaluation.", ex);
+        }
+    }
+
+    /**
+     * 
+     * Update a taste evaluation based on the given id.
+     *
+     * @param id The id of the taste evaluation to update.
+     * @param updateTasteEvaluationDto The data to update the taste evaluation with.
+     *
+     * @return The updated taste evaluation.
+     * 
+     */
+    public ResponseTasteEvaluationDto Update(Guid id, UpdateTasteEvaluationDto updateTasteEvaluationDto)
+    {
+        var tasteEvaluation = _context.TasteEvaluations.Find(id);
+
+        if (tasteEvaluation == null)
+        {
+            throw new ArgumentException("Error: That taste evaluation does not exist!");
+        }
+
+        tasteEvaluation.UserId = updateTasteEvaluationDto.UserId;
+        tasteEvaluation.WineId = updateTasteEvaluationDto.WineId;
+        
+        // TODO: Add the event
+        // tasteEvaluation.EventId = updateTasteEvaluationDto.EventId;
+        
+        tasteEvaluation.WineScore = updateTasteEvaluationDto.WineScore;
+
+        _context.SaveChanges();
+
+        return new ResponseTasteEvaluationDto
+        {
+            TasteEvaluationId = tasteEvaluation.TasteEvaluationId,
+            UserId = tasteEvaluation.UserId,
+            WineId = tasteEvaluation.WineId,
+            
+            // TODO: Add the event
+            // EventId = tasteEvaluation.EventId,
+            
+            WineScore = tasteEvaluation.WineScore
+        };
+    }
+
+    /**
+     *
+     * Delete a taste evaluation based on the given id.
+     *
+     * @param id The id of the taste evaluation to delete.
+     * 
+     */
+    public void Delete(Guid id)
+    {
+        using (var transaction = _context.Database.BeginTransaction())
+        {
+            try
+            {
+                var tasteEvaluation = _context.Brands.Find(id);
+
+                if (tasteEvaluation == null)
+                {
+                    throw new ArgumentException("Error: That taste evaluation does not exist!");
+                }
+
+                tasteEvaluation.DeletedAt = DateTimeOffset.UtcNow; 
+                _context.SaveChanges();
+                
+                // if everything is correct, just commit the transaction
+                transaction.Commit();
+            }
+            catch (Exception ex)
+            {
+                transaction.Rollback();
+                
+                throw new Exception("Error: An error occurred while trying to delete the TasteEvaluation.", ex);
+            }
+        }
+    }
+}
