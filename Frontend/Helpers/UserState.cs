a5fbeabdc5a551d889b89dc01db8a42983502528
Fernando Rocha <frocha2910@gmail.com>
2024-06-24 17:35:31 +0100

    QRCode working: registering as participant & checking if its participant already & not logged in check (missing login redirect when not logged in)

--- Frontend/Helpers/UserState.cs
+++ Frontend/Helpers/UserState.cs
@@ -7,7 +7,7 @@
         public static Guid? LoggedInUserId = new Guid("5b50f84e-0cde-43e8-8048-8efdbff24811");
         
         // Not logged in
-        // public static Guid? LoggedInUserId = new Guid("00000000-0000-0000-0000-000000000000");
+        //public static Guid? LoggedInUserId = new Guid("00000000-0000-0000-0000-000000000000");
         // or
         // public static Guid LoggedInUserId = Guid.Empty;
 
