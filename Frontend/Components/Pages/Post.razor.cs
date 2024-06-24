3d5ada66bd5a7de4ba03a3acfdc25fe1dd3992a5
Diogo Marques <randombidoof@gmail.com>
2024-06-24 20:19:19 +0100

    ajustar variaveis e remover code-behind

--- Frontend/Components/Pages/Post.razor.cs
+++
@@ -1,9 +0,0 @@
-﻿using Helpers.ViewModels;
-using Microsoft.AspNetCore.Components;
-
-namespace Frontend.Components.Pages;
-
-public partial class Post
-{
-    [Parameter] public FeedPost PostLine { get; set; } = new FeedPost();
-}
