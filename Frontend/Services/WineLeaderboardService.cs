024c5013922ae1374b94a6e3b0d534afd5ab9bc4
MistaDjango <pauloalexccarvalho@gmail.com>
2024-06-26 22:10:25 +0100

    rename some methods

--- Frontend/Services/WineLeaderboardService.cs
+++ Frontend/Services/WineLeaderboardService.cs
@@ -15,17 +15,17 @@ namespace Frontend.Services
             _apiHelper = apiHelper;
         }
 
-        public async Task<List<WineLeaderboardDto>> GetLeaderboardAsync()
+        public async Task<List<WineLeaderboardDto>> GetWineLeaderboardAsync()
         {
             var result = await _apiHelper.GetFromApiAsync<List<WineLeaderboardDto>>("api/wineleaderboard/leaderboard");
             return result ?? new List<WineLeaderboardDto>();
         }
 
-        public async Task<List<WineLeaderboardDto>> GetLeaderboardByRegionAsync(Guid? regionId)
+        public async Task<List<WineLeaderboardDto>> GetWineLeaderboardByRegionAsync(Guid? regionId)
         {
             if (regionId == null)
             {
-                return await GetLeaderboardAsync();
+                return await GetWineLeaderboardAsync();
             }
             var result = await _apiHelper.GetFromApiAsync<List<WineLeaderboardDto>>($"api/wineleaderboard/leaderboard/byregion/{regionId}");
             return result ?? new List<WineLeaderboardDto>();
