38748340383a215ece563fe571c0762373ece9d5
Diogo Marques <randombidoof@gmail.com>
2024-06-28 08:01:13 +0100

    pesquisar por hashtag e username

--- Frontend/Helpers/UserState.cs
+++ Frontend/Helpers/UserState.cs
@@ -4,7 +4,7 @@
     public static class UserState
     {
         // logged in fake
-        public static Guid? LoggedInUserId = new Guid("9d957de9-df3a-4c68-9a93-617b43b1bcfd");
+        public static Guid? LoggedInUserId = new Guid("5b50f84e-0cde-43e8-8048-8efdbff24811");
         
         // Not logged in
         //public static Guid? LoggedInUserId = new Guid("00000000-0000-0000-0000-000000000000");
