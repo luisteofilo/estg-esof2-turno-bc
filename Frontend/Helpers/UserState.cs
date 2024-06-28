91b872edb31f052f9b41f57eb6ed1d628dcf3911
Xicojfc <crfcorreia@gmail.com>
2024-06-28 12:15:02 +0100

    Blind Events Updated

--- Frontend/Helpers/UserState.cs
+++ Frontend/Helpers/UserState.cs
@@ -4,7 +4,7 @@ namespace Frontend.Helpers
     public static class UserState
     {
         // logged in fake
-        public static Guid? LoggedInUserId = new Guid("5b50f84e-0cde-43e8-8048-8efdbff24811");
+        //public static Guid? LoggedInUserId = new Guid("5b50f84e-0cde-43e8-8048-8efdbff24811");
         // logged in fake
         //public static Guid? LoggedInUserId = new Guid("c5b93934-36f0-4a7e-bc9d-e5617a543f25");
         // Not logged in
@@ -12,7 +12,7 @@ namespace Frontend.Helpers
         // or
         // public static Guid LoggedInUserId = Guid.Empty;
         //logged in fake 2
-        //public static Guid? LoggedInUserId = new Guid("5085d3ab-05d3-4643-9632-331b856daefe");
+        public static Guid? LoggedInUserId = new Guid("5085d3ab-05d3-4643-9632-331b856daefe");
         // logged in fake 3
         //public static Guid? LoggedInUserId = new Guid("1a950cd6-cb12-4b12-ae72-357b6c1eacfc");
         // logged in fake 4
